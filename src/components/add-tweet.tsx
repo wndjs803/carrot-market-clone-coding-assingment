ea347ae16b103128237be0118067835d43b73662
김주언 <wndjs803@naver.com>
2024-12-16 01:57:11 +0900

    add-tweets 수정

--- src/components/add-tweet.tsx
+++ src/components/add-tweet.tsx
@@ -9,10 +9,17 @@ export default function AddTweet() {
   return (
     <form action={action} className="p-5 flex flex-col gap-5">
       <div className="flex flex-col gap-2">
-        <textarea name="tweet" required placeholder="Write a tweet" className="w-full p-5 rounded-md resize-none" />
-        {!state?.isSuccess && <span className="text-red-400">{state?.error.fieldErrors.tweet}</span>}
+        <textarea
+          name="tweet"
+          required
+          placeholder="트윗을 작성해 주세요."
+          className="w-full p-5 rounded-md resize-none"
+        />
+        {!state?.isSuccess && (
+          <span className="text-red-400">{state?.error.fieldErrors.tweet}</span>
+        )}
       </div>
-      <Button text="Add tweet" />
+      <Button text="트윗 추가" />
     </form>
   );
 }
